05d050f26db52977115823e4784153fe7e233764
eisenwj <63277292+eisenwj@users.noreply.github.com>
2022-05-17 11:50:50 +0800

    kid_book_downloader.py updated
    Add some more img filter urls

--- kid_book_downloader.py
+++ kid_book_downloader.py
@@ -18,6 +18,7 @@ from docx import Document
 from docx.enum.section import *
 from docx.enum.text import WD_PARAGRAPH_ALIGNMENT
 from docx.shared import Cm, Inches
+from docx.image.exceptions import *
 
 
 #get the kidbook's title
@@ -82,6 +83,7 @@ def page_parser(url):
 #if is advertise -- return true, else return false
 def img_filter(url_str):
     adv_list=[
+        r"http://mmbiz.qpic.cn/mmbiz/wHtT7l3B4aAW3OcBzKXH6yibzoMgKrSMZOS99xQaD0uwRdiaTTH8ibSApDhuTZ6WBMqJgM8ZtMJ6qiaYjCtzWxlU7Q/0?wxfrom=5&wx_fmt=gif",
         r"https://mmbiz.qpic.cn/mmbiz_gif/xvjlzfjg30HfgVpxOmBXv3Wib4qDXFH8sanWcl7605GBD09PcSwIial58iaQ4fqzdvXHAbIe2YfsF9RAKWrO3eISw/640?wx_fmt=gif",
         r"https://mmbiz.qpic.cn/mmbiz_png/wHtT7l3B4aCrjUWduXMEPZEwRQucZpfjvxvyOXmJXVYP5nDKqicW69Y396k4Ps0uVq8HmHZjB1D592dmuoJDHWQ/640?wx_fmt=jpeg",
         r"https://mmbiz.qpic.cn/mmbiz_jpg/wHtT7l3B4aADm9QHjicSv7ejN0lHNJUKuQAMXIK0xXxdSyMGvVxB5fYx2VK40z8icvOW6rh9mIlichibpQzndZzlRQ/640?wx_fmt=jpeg",
@@ -90,7 +92,10 @@ def img_filter(url_str):
         r"https://mmbiz.qpic.cn/mmbiz_jpg/jR58jqMGnUZSdAHRAcmVWBbXzVDpicpvZ8PYvtneibicybticMicuE9ia70MDUkTVA4Bgf9NoPDIL70icPibuVz6ibnO94A/640?wx_fmt=jpeg",
         r"https://mmbiz.qpic.cn/mmbiz_png/xvjlzfjg30FZJf153icaVD901QsA9LMAjJh0bqHyfY7EAKIiaP2DicynIFHTarN4QsiaHYYmmiaQyZ0XuC1t4JW6gPg/640?wx_fmt=png",
         r"https://mmbiz.qpic.cn/mmbiz_png/xvjlzfjg30FicI1vxgZQBAU7OicZORSrCEo0WqhXvib714QwEW7g2bcQliapA2abhXqRgyQqV0WGFmhFRg3bwg8Cog/640?wx_fmt=png",
-        r"https://mmbiz.qpic.cn/mmbiz_jpg/6bML8pV2ozEcG6JV3zgb6ibOJsxic0ic8vib12RCDwW957m2WciazBUNiblUt8cuefUo2aa6XibGz7xZSM52ZnE9r4FAQ/640?wx_fmt=jpeg"
+        r"https://mmbiz.qpic.cn/mmbiz_jpg/6bML8pV2ozEcG6JV3zgb6ibOJsxic0ic8vib12RCDwW957m2WciazBUNiblUt8cuefUo2aa6XibGz7xZSM52ZnE9r4FAQ/640?wx_fmt=jpeg",
+        r"http://mmbiz.qpic.cn/mmbiz/wHtT7l3B4aDymGIgvnib6KouFAAk8HSDfxHNfQDwlLPhr02WVyOF7ZYdxXjESBibeqDtEdWo1THcyOuT1xF76CWA/640?wx_fmt=jpeg",
+        r"http://mmbiz.qpic.cn/mmbiz/wHtT7l3B4aAvz6sHVz7uq5I8BAjH23QI7wp8PF9FP7kL3x5iaLgHPJhtgSqc5q9MAOAsyGjicRA7478O2ia0gicItg/640?wx_fmt=png",
+        r"http://mmbiz.qpic.cn/mmbiz/wHtT7l3B4aC88sEefGxFhofjuh9Nr2AhibkGJuK0T1kaSnefVibduibCy1u80qrg0xdTV60p5aGQUbsBib7GOCticHw/640?wx_fmt=jpeg"
         ]
     if (url_str in adv_list):
         return True
@@ -156,7 +161,7 @@ def kidbook_download(kidbook_url,errormsgs,configures):
         pic_url=str(imgs[i].get("data-src"))
         print(str(i)+":   "+book_title+"    "+pic_url)
         if ((pic_url == "None") 
-            or (pic_url.find(r"640?wx_fmt=")==-1)
+#            or (pic_url.find(r"640?wx_fmt=")==-1)
             or (img_filter(pic_url))
 #            or (pic_url.find(r"wx_fmt=png")!=-1)
 #            or (pic_url.find(r"640?tp=webp")!=-1)
@@ -172,15 +177,20 @@ def kidbook_download(kidbook_url,errormsgs,configures):
 
             if (flag == 0):
                 wh_ratio=float(img_validate(img_dir+os.sep+"640_"+str(i)+".jpg",rotate_mode))
+                try:
                 #A4 paper size is 297*210
-                if (rotate_mode > 0 ):
-                    if wh_ratio > float(297.0/210.0):
-                        run.add_picture(img_dir+os.sep+"640_"+str(i)+".jpg",width=Inches(11))
+                    if (rotate_mode > 0 ):
+                        if wh_ratio > float(297.0/210.0):
+                            run.add_picture(img_dir+os.sep+"640_"+str(i)+".jpg",width=Inches(11))
+                        else:
+                            run.add_picture(img_dir+os.sep+"640_"+str(i)+".jpg",height=Inches(8))
                     else:
-                        run.add_picture(img_dir+os.sep+"640_"+str(i)+".jpg",height=Inches(8))
-                else:
-                    run.add_picture(img_dir+os.sep+"640_"+str(i)+".jpg",height=Inches(11))
-                break
+                        run.add_picture(img_dir+os.sep+"640_"+str(i)+".jpg",height=Inches(11))
+                    break
+                except docx.image.exceptions.UnexpectedEndOfFileError:
+                    print("640_"+str(i)+".jpg failed to add to doc.")
+                    errormsgs.append("640_"+str(i)+".jpg failed to add to doc.")
+                    break
             else:
                 print("640_"+str(i)+".jpg failed for "+str(j)+" times.")
                 if (j == 4):
